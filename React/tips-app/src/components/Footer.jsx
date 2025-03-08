85660d26f3515655d87801a6fd9137eebc1c2d35
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2025-03-08 19:52:00 -0300

    fix style

--- React/tips-app/src/components/Footer.jsx
+++ React/tips-app/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ano = new Date().getFullYear();
+const year = new Date().getFullYear();
 function Footer() {
     return (
         <footer>
-            <p>Copyright Fabio Abreu @{ano}</p>
+            <p>Copyright ©{year}</p>
         </footer>
     )
 }
